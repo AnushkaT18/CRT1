d0556edd0696a0e93b9509ea13f7afc96e46d863
AnushkaT18 <anushkataksande37@gmail.com>
2026-06-06 10:41:38 +0530

    second update

---
+++ Day11_CRT/11.1.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    int num;
+int a[]={aaabbbccc};
+int s=sizeof(arr)/sizeof(arr[0]);
+for (int i=0;i<=s;i++){
+    if(arr[0]==arr[i]{
+       num++;
+       })
+}
+
---
+++ Day11_CRT/11.1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int num;
+char arr[]={"aaabbbccc"};
+int s=sizeof(arr)/sizeof(arr[0]);
+for (int i=0;i<=s;i++){
+    if(arr[0]==arr[i]){
+       num++;
+}
+else if(!arr[0]==arr[i]){
+
+    if( if(arr[i]==arr[i]){
+       num++;
+
+}
+}
+cout<<arr[0]<<num;
+num=0;
+}
